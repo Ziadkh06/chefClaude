9ed2dbefe9b956e195722dd06e528b671e218e45
Ziadkh06 <136996701+Ziadkh06@users.noreply.github.com>
2026-08-13 20:07:47 +0300

    form logic, handling sbumit and updating the ingredients list

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,35 @@
+import React from 'react'
+
 export default function Main() {
+
+  const [ingredients, setIngredients] = React.useState([])
+
+  const ingredientsList = ingredients.map((ingredient, index) => (
+    <li key={index}>{ingredient}</li>
+  ))
+
+  function handleSubmit(event){
+
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
+    const newIngredient = formData.get("ingredient")
+    setIngredients(prevArr => [...prevArr, newIngredient])
+  }
+
   return (
     <main> 
-        <form className="add-ingredient-form">
+        <form onSubmit={handleSubmit} className="add-ingredient-form">
             <input  
                 type="text"
                 placeholder="e.g. oregano"
                 aria-label="Add ingredient"
+                name="ingredient"
             />
             <button type="submit">Add Ingredient</button>
         </form>
+        <ul>
+            {ingredientsList}
+        </ul>
     </main>
   )
 }
